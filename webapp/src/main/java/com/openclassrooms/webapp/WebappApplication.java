255361b3aafc40fa5f480d2ac0ccfb812d75d667
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-08-30 19:04:22 +0400

    webapp

--- webapp/src/main/java/com/openclassrooms/webapp/WebappApplication.java
+++ webapp/src/main/java/com/openclassrooms/webapp/WebappApplication.java
@@ -1,13 +1,25 @@
 package com.openclassrooms.webapp;
 
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//public class WebappApplication implements CommandLineRunner
 public class WebappApplication {
 
+	// @Autowired
+	// private CustomProperties props;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
+	// @Override
+	// public void run(String... args) throws Exception{
+	// 	System.out.println(props.getApiUrl());
+	// }
+
 }
+
